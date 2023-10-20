c67defe354fb70e4e9ed6171b77e7d034d3c23eb
Buster Hultgren Wärn <buster.hultgrenwarn@tietoevry.com>
2023-10-20 09:09:11 +0200

    Hellow World

---
+++ hackerrank/hello_world/README.md
@@ -0,0 +1,5 @@
+## Hello World!
+
+Very Simple `:)`
+
+https://www.hackerrank.com/challenges/fp-hello-world/problem
---
+++ hackerrank/hello_world/solution.exs
@@ -0,0 +1,11 @@
+defmodule Solution do
+  # Define a function that prints "Hello World" to STDOUT
+  def hello_world do
+      # Use IO.puts to print a string
+      IO.puts("Hello World")
+  end
+end
+
+
+# Call the hello_world function
+Solution.hello_world()
